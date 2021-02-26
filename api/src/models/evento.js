76779942b08a587f00e744bb9e506fce1b8c7018
tr0b <trobinson3000600@gmail.com>
2021-02-25 21:28:20 -0600

    Mongoose models - Will add evento Model Changes Include: - Added Evento Model to models list. - Added initial documentation for Evento. (description and example)

---
+++ api/src/models/evento.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+
+/**
+* @description Representacion de un Evento
+* Ejemplo: { codigo: EVE-01, nombre: Creacion de usuario X }
+* */
+const EventoSchema = new Schema({
+  codigo: { type: String },
+  nombre: { type: String }
+})
+
+module.exports = mongoose.model('Evento', EventoSchema)
