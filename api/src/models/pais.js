c15d7e102ccb6f58cffc730f1ecb861dc11b4834
tr0b <trobinson3000600@gmail.com>
2021-02-25 21:24:10 -0600

    Mongoose Models _ Will add documentation to Pais Changes include: - Added documentation to Pais Schema (description and properties)

--- api/src/models/pais.js
+++ api/src/models/pais.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose')
 const { Schema } = mongoose
 
+/**
+* @description PaisSchema. Tiene codigo y nombre
+* Ejemplo: { codigo: PAI-01, nombre: Portugal }
+* */
 const PaisSchema = new Schema({
   codigo: { type: String },
   nombre: { type: String }
