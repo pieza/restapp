9ed5eccdc1518b4cc39da4d4806f02855f074470
tr0b <trobinson3000600@gmail.com>
2021-02-26 21:01:16 -0600

    Mongoose models - Will refactor Buffet Model Changes Include: - imported directly objectId from mongoose schema types - refactored objectid type call in medida property

--- api/src/models/buffet.js
+++ api/src/models/buffet.js
@@ -1,14 +1,16 @@
-const mongoose = require('mongoose')
-const { Schema } = mongoose
+const mongoose = require('mongoose');
+
+const { ObjectId } = mongoose.Schema.Types;
+const { Schema } = mongoose;
 
 /** @description Representacion de un Buffet de un Restaurante */
 const BuffetSchema = new Schema({
   codigo: { type: String },
   nombre: { type: String },
   tipo: { type: String },
-  medida: { type: moongose.ObjectId },
+  medida: { type: ObjectId, ref: 'Medida' },
   precio: { type: Number },
   imagen: { type: String },
-})
+});
 
-module.exports = mongoose.model('Buffet', BuffetSchema)
+module.exports = mongoose.model('Buffet', BuffetSchema);
