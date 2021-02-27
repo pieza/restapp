e6eb371a188a8e704c44dbb96e05addd67cd7cdc
tr0b <trobinson3000600@gmail.com>
2021-02-26 21:09:59 -0600

    Mongoose Models - Will add Initial Factura Mongoose Model Changes Include: - Added Initial Factura Mongoose Model

---
+++ api/src/models/factura.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const { ObjectId } = mongoose.Schema.Types;
+const { Schema } = mongoose;
+
+/** @description Representacion de una Factura de un Restaurante */
+const FacturaSchema = new Schema({
+  codigo: { type: String },
+  fecha: { type: Date },
+  monto: { type: Number },
+  buffets: [{ type: ObjectId, ref: 'Buffet' }],
+  bebidas: [{ type: ObjectId, ref: 'Bebida' }],
+  comestibles: [{ type: ObjectId, ref: 'Comestible' }],
+  especiales: [{ type: ObjectId, ref: 'Especial' }],
+  cliente: { type: ObjectId, ref: 'Cliente' },
+});
+
+module.exports = mongoose.model('Factura', FacturaSchema);
