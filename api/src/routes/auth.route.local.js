cbae5f36705b2bf31d91650a1a7c89ee9f75ae51
tr0b <trobinson3000600@gmail.com>
2021-03-13 12:57:34 -0600

    Auth - Will fix Sign Up and Sign In endpoints Changes: - Fixed Sign Up route not saving new user correctly due to receiving an   undefined object - Fixed login route not throwing errors correctly, when faced with wrong   password/username - Changed error status code for login to 401, when related to invalid   credentials

--- api/src/routes/auth.route.local.js
+++ api/src/routes/auth.route.local.js
@@ -11,15 +11,19 @@ const Usuario = require('../models/usuario.js');
 
 // [POST] Sign Up Usuario
 router.post('/signup', (req, res) => {
-  new Usuario(req.body).save().then(res.status(201).send).catch((e) => res.status(400).send(e));
+  const usuario = new Usuario(req.body);
+  usuario.save().then(() => res.status(201).send())
+    .catch((e) => res.status(500).send({ error: e.message }));
 });
 
 // [POST] Login Usuario
 router.post('/signin', (req, res, next) => {
   passport.authenticate('local', (err, usuario, info) => {
     if (err) return res.status(501).json(err);
-    if (!usuario) return res.status(501).json(info);
-    req.logIn(usuario, (err) => { if (err) return res.status(501).json(err); });
+    if (!usuario) return res.status(401).json(info);
+    req.logIn(usuario, (err) => {
+      if (err) return res.status(501).json(err);
+    });
     return res.status(200).json(usuario);
   })(req, res, next);
 });
