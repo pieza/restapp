6b90449a75295b6dff9b4f03762c8197f4524153
tr0b <trobin3000600@gmail.com>
2021-03-27 14:59:07 -0600

    Auth FrontEnd - Will create initial login form Changes: - Created initial login form in frontend

--- admin/pages/auth/login.js
+++ admin/pages/auth/login.js
@@ -23,44 +23,9 @@ function Login() {
     <>
       <Col lg="5" md="7">
         <Card className="bg-secondary shadow border-0">
-          <CardHeader className="bg-transparent pb-5">
-            <div className="text-muted text-center mt-2 mb-3">
-              <small>Sign in with</small>
-            </div>
-            <div className="btn-wrapper text-center">
-              <Button
-                className="btn-neutral btn-icon"
-                color="default"
-                href="#pablo"
-                onClick={(e) => e.preventDefault()}
-              >
-                <span className="btn-inner--icon">
-                  <img
-                    alt="..."
-                    src={require("assets/img/icons/common/github.svg")}
-                  />
-                </span>
-                <span className="btn-inner--text">Github</span>
-              </Button>
-              <Button
-                className="btn-neutral btn-icon"
-                color="default"
-                href="#pablo"
-                onClick={(e) => e.preventDefault()}
-              >
-                <span className="btn-inner--icon">
-                  <img
-                    alt="..."
-                    src={require("assets/img/icons/common/google.svg")}
-                  />
-                </span>
-                <span className="btn-inner--text">Google</span>
-              </Button>
-            </div>
-          </CardHeader>
           <CardBody className="px-lg-5 py-lg-5">
             <div className="text-center text-muted mb-4">
-              <small>Or sign in with credentials</small>
+              <small>Sign In</small>
             </div>
             <Form role="form">
               <FormGroup className="mb-3">
@@ -71,8 +36,8 @@ function Login() {
                     </InputGroupText>
                   </InputGroupAddon>
                   <Input
-                    placeholder="Email"
-                    type="email"
+                    placeholder="Usuario"
+                    type="username"
                     autoComplete="new-email"
                   />
                 </InputGroup>
@@ -91,23 +56,8 @@ function Login() {
                   />
                 </InputGroup>
               </FormGroup>
-              <div className="custom-control custom-control-alternative custom-checkbox">
-                <input
-                  className="custom-control-input"
-                  id=" customCheckLogin"
-                  type="checkbox"
-                />
-                <label
-                  className="custom-control-label"
-                  htmlFor=" customCheckLogin"
-                >
-                  <span className="text-muted">Remember me</span>
-                </label>
-              </div>
               <div className="text-center">
-                <Button className="my-4" color="primary" type="button">
-                  Sign in
-                </Button>
+                <Button className="my-4" color="primary" type="button">Ingresar</Button>
               </div>
             </Form>
           </CardBody>
@@ -119,16 +69,15 @@ function Login() {
               href="#pablo"
               onClick={(e) => e.preventDefault()}
             >
-              <small>Forgot password?</small>
+              <small>Olvide mis credenciales</small>
             </a>
           </Col>
           <Col className="text-right" xs="6">
             <a
               className="text-light"
-              href="#pablo"
-              onClick={(e) => e.preventDefault()}
+              href="/auth/register"
             >
-              <small>Create new account</small>
+              <small>Registrarse</small>
             </a>
           </Col>
         </Row>
