ebed7681a5f5d365517fba51779c82ef3e3cdc66
tr0b <trobin3000600@gmail.com>
2021-04-17 01:06:02 -0600

    Client Stats Dashboard - Added Initial Client Dashboard statistics

--- admin/components/Headers/HeaderClient.js
+++ admin/components/Headers/HeaderClient.js
@@ -1,9 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import {
+  Card, CardBody, CardTitle, Container, Row, Col,
+} from 'reactstrap';
+import AuthService from '../../services/auth.service';
+import RestauranteService from '../../services/restaurante.service';
+import MesaService from '../../services/mesa.service';
+import ClienteService from '../../services/cliente.service';
+import ProductoService from '../../services/producto.service';
 
 // reactstrap components
-import { Card, CardBody, CardTitle, Container, Row, Col } from "reactstrap";
 
 function HeaderClient() {
+  const [user, setUser] = useState({});
+  const [restaurante, setRestaurante] = useState({});
+  const [mesas, setMesas] = useState([]);
+  const [clientes, setClientes] = useState([]);
+  const [productos, setProductos] = useState(0);
+  const restauranteService = new RestauranteService();
+  const mesaService = new MesaService();
+  const clienteService = new ClienteService();
+  const productoService = new ProductoService();
+  useEffect(async () => {
+    const _user = await AuthService.current();
+    if (_user) {
+	    setUser(_user);
+	    const restaurante_id = _user.empleado.restaurante;
+	    const restaurante = await restauranteService.findById(restaurante_id);
+	    if (restaurante) setRestaurante(restaurante);
+	    const mesas = await mesaService.find({ restaurante: restaurante_id });
+	    if (mesas.length) setMesas(mesas);
+	    const clientes = await clienteService.find({ restaurante: restaurante_id });
+	    if (clientes.length) setClientes(clientes);
+	    const productos = await productoService.find({ restaurante: restaurante_id });
+	    const totalProductos = productos.reduce((previous, it) => previous += parseInt(it.cantidad), 0);
+	    setProductos(totalProductos)
+    }
+  }, []);
   return (
     <>
       <div className="header bg-gradient-dark pb-8 pt-5 pt-md-8">
@@ -20,11 +52,9 @@ function HeaderClient() {
                           tag="h5"
                           className="text-uppercase text-muted mb-0"
                         >
-                          Traffic
+                          Clientes
                         </CardTitle>
-                        <span className="h2 font-weight-bold mb-0">
-                          350,897
-                        </span>
+                        <span className="h2 font-weight-bold mb-0">{clientes.length}</span>
                       </div>
                       <Col className="col-auto">
                         <div className="icon icon-shape bg-danger text-white rounded-circle shadow">
@@ -33,10 +63,7 @@ function HeaderClient() {
                       </Col>
                     </Row>
                     <p className="mt-3 mb-0 text-muted text-sm">
-                      <span className="text-success mr-2">
-                        <i className="fa fa-arrow-up" /> 3.48%
-                      </span>{" "}
-                      <span className="text-nowrap">Since last month</span>
+                      {' '}
                     </p>
                   </CardBody>
                 </Card>
@@ -50,9 +77,9 @@ function HeaderClient() {
                           tag="h5"
                           className="text-uppercase text-muted mb-0"
                         >
-                          New users
+                          Mesas Disponibles
                         </CardTitle>
-                        <span className="h2 font-weight-bold mb-0">2,356</span>
+                        <span className="h2 font-weight-bold mb-0">{mesas.length}</span>
                       </div>
                       <Col className="col-auto">
                         <div className="icon icon-shape bg-warning text-white rounded-circle shadow">
@@ -61,10 +88,7 @@ function HeaderClient() {
                       </Col>
                     </Row>
                     <p className="mt-3 mb-0 text-muted text-sm">
-                      <span className="text-danger mr-2">
-                        <i className="fas fa-arrow-down" /> 3.48%
-                      </span>{" "}
-                      <span className="text-nowrap">Since last week</span>
+                      {' '}
                     </p>
                   </CardBody>
                 </Card>
@@ -78,9 +102,9 @@ function HeaderClient() {
                           tag="h5"
                           className="text-uppercase text-muted mb-0"
                         >
-                          Sales
+                          Mesas Ocupadas
                         </CardTitle>
-                        <span className="h2 font-weight-bold mb-0">924</span>
+                        <span className="h2 font-weight-bold mb-0">{mesas.length - mesas.filter((m) => m.estado === 'disponible').length}</span>
                       </div>
                       <Col className="col-auto">
                         <div className="icon icon-shape bg-yellow text-white rounded-circle shadow">
@@ -89,10 +113,7 @@ function HeaderClient() {
                       </Col>
                     </Row>
                     <p className="mt-3 mb-0 text-muted text-sm">
-                      <span className="text-warning mr-2">
-                        <i className="fas fa-arrow-down" /> 1.10%
-                      </span>{" "}
-                      <span className="text-nowrap">Since yesterday</span>
+                      {' '}
                     </p>
                   </CardBody>
                 </Card>
@@ -106,9 +127,9 @@ function HeaderClient() {
                           tag="h5"
                           className="text-uppercase text-muted mb-0"
                         >
-                          Performance
+                          Productos
                         </CardTitle>
-                        <span className="h2 font-weight-bold mb-0">49,65%</span>
+                        <span className="h2 font-weight-bold mb-0">{productos}</span>
                       </div>
                       <Col className="col-auto">
                         <div className="icon icon-shape bg-info text-white rounded-circle shadow">
@@ -117,10 +138,7 @@ function HeaderClient() {
                       </Col>
                     </Row>
                     <p className="mt-3 mb-0 text-muted text-sm">
-                      <span className="text-success mr-2">
-                        <i className="fas fa-arrow-up" /> 12%
-                      </span>{" "}
-                      <span className="text-nowrap">Since last month</span>
+                      {' '}
                     </p>
                   </CardBody>
                 </Card>
